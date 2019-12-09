88da920bd44d590b56e5016d30196dad79edddb6
doubix <adib.grouz@gmail.com>
2019-12-09 15:39:14 +0100

    10.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if list has a cycle
+ * @head: pointer to list to be freed
+ * Return: 0 if has no cycle or 1 if has cycle
+ */
+int check_cycle(listint_t *head)
+{
+listint_t *next;
+next = head->next;
+while (next != NULL && next != head)
+next = next->next;
+return (next == head);
+}
