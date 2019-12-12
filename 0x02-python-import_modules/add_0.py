dc8f264a0eb0a41bd8f723ca833994f2f97807a6
student1043 <1043@holbertonschool.com>
2019-12-12 09:24:01 +0100

    Task 0-1

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
+if __name__ == "__main__":
+    def add(a, b):
+        """My addition function
 
-    Args:
-        a: first integer
-        b: second integer
+        Args:
+            a: first integer
+            b: second integer
 
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+        Returns:
+            The return value. a + b
+        """
+        return (a + b)
