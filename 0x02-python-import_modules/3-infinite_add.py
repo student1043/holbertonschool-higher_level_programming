91e6418227a451de5e21c7c4d0c5a00826ba9009
student1043 <1043@holbertonschool.com>
2019-12-12 15:07:05 +0100

    Fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,9 @@
 import sys
 num = 0
 if __name__ == "__main__":
-    for i in range(1, len(sys.argv)):
-        num += int(sys.argv[i])
-    print(num)
+    if len(sys.argv) - 1 == 0:
+        print("0")
+    else:
+        for i in range(1, len(sys.argv)):
+            num += int(sys.argv[i])
+        print(num)
