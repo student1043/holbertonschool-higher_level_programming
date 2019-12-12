3388ea99d8b3ca0f579cca2d168e11150eec9988
student1043 <1043@holbertonschool.com>
2019-12-12 15:05:53 +0100

    Fixing

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+import sys
 num = 0
-
 if __name__ == "__main__":
-    import sys
     for i in range(1, len(sys.argv)):
         num += int(sys.argv[i])
-    print("{:d}".format(num))
+    print(num)
