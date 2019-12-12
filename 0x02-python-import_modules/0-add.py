f4047ae68cdd20ad73cbcab6aec754b9e803d641
student1043 <1043@holbertonschool.com>
2019-12-12 09:35:58 +0100

    Adding __name__ and __main__

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from add_0 import add
+add = __import__('add_0')
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, add.add(a, b)))
--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
 if __name__ == "__main__":
-    def add(a, b):
-        """My addition function
-
-        Args:
-            a: first integer
-            b: second integer
-
-        Returns:
-            The return value. a + b
-        """
-        return (a + b)
+    add()
