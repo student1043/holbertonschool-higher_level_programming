b8d0e013e677f7467c98fded1fd8308cf6fd72c8
student1043 <1043@holbertonschool.com>
2019-12-17 13:01:58 +0100

    TAsk 3

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    list = my_list
+    if idx < 0:
+        return (list)
+    elif idx >= len(my_list):
+        return (list)
+    else:
+        list[idx] = element
+        return (list)
