4e8825c3aae6a0cc375b2ac0d9299c7363c0c4d5
student1043 <1043@holbertonschool.com>
2019-12-17 12:36:47 +0100

    Task 0-1-2

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+
+}
