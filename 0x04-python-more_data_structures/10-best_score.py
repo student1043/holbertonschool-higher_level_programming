9d5f9b5c111271bc38943458562327201b56b596
student1043 <1043@holbertonschool.com>
2019-12-19 17:16:36 +0100

    All mandatory tasks

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        maxkey = max(a_dictionary, key=a_dictionary.get)
+    else:
+        return (None)
+    return (maxkey)
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string and type(roman_string) is str:
+        r = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        int_num = 0
+        for i in roman_string:
+            if i not in r.keys():
+                return 0
+            int_num += r[i]
+        return int_num
+    return (0)
