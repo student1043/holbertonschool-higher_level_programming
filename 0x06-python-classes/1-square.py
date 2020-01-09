5142a3c6d141ed120e5b517160f3b0105150c55e
student1043 <1043@holbertonschool.com>
2020-01-09 19:49:25 +0100

    Task 0

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,5 +3,5 @@ class Square:
     """This is a square class"""
     def __init__(self, size):
         """This is a definition"""
-        self.__size = 3
+        self.__size = size
         """This is an attribute"""
