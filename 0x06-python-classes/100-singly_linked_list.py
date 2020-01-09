7807364576ae67426683e85b37e17b75c442d356
student1043 <1043@holbertonschool.com>
2020-01-09 22:28:11 +0100

    Fixing Task 100

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -7,7 +7,7 @@ class Node:
         self.__next_node = next_node
         if not isinstance(data, int):
             raise TypeError("data must be an integer")
-        if not isinstance(next_node, Node) or next_node != None
+        if not isinstance(next_node, Node) or next_node != None:
             raise TypeError("next_node must be a Node object")
 
     @property
@@ -22,4 +22,23 @@ class Node:
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
 
+    @property
+    def next_node(self):
+        """This is a definition"""
+        return self.__next_node
     
+    @next_node.setter
+    def next_node(self, value):
+        """This is a definition"""
+        self.__next_node = value
+        if not isinstance(next_node, Node) or next_node != None:
+            raise TypeError("next_node must be a Node object")
+class SinglyLinkedList:
+    """This is a class"""
+    def __init__(self):
+        """This is a definition"""
+        self.__head = self.__data
+        print(self.__head)
+    def sorted_insert(self, value):
+        """This is a definition"""
+        self.__next_node = value
