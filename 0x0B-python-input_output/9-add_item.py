b92e744ce5bcf049ced5b9aafa908040089c5762
student1043 <1043@holbertonschool.com>
2020-01-22 23:14:48 +0100

    Fixing Task 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 import json
 import sys
-import os.path
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+import os
+loadmedaddy = __import__('8-load_from_json_file').load_from_json_file
+savemedaddy = __import__('7-save_to_json_file').save_to_json_file
 
-
-list = []
 filename = "add_item.json"
-if os.path.exists(filename):
-    list = load_from_json_file(filename)
-for i in range(len(sys.argv)):
+try:
+    object = loadmedaddy(filename)
+except:
+    savemedaddy([], filename)
+list = []
+for i in range(len(os.sys.argv)):
     if i == 0:
         continue
-    list.append(sys.argv[i])
-save_to_json_file(list, filename)
+    list.append(os.sys.argv[i])
+savemedaddy(list, filename)
