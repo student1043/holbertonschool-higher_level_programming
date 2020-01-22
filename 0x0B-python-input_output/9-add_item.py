21048b9e670f1cd88f51bef4d49717f22ab11f8a
student1043 <1043@holbertonschool.com>
2020-01-22 23:40:44 +0100

    Fixingggggg

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-import json
-import os
-l = __import__('8-load_from_json_file').load_from_json_file
-s = __import__('7-save_to_json_file').save_to_json_file
-
 """
-Adding arguments to a JSON file add_item.json
+Adding all arguments to JSON File
 """
-
-filename = "add_item.json"
+import sys
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 try:
-    object = l(filename)
+    thisobject = load_from_json_file("add_item.json")
 except:
-    with open(filename, mode='w', encoding='UTF8') as pr:
-        pr.write('')
-        object = []
-if len(os.sys.argv) == 1:
-    s(object, filename)
-else:
-    for i in range(1, len(os.sys.argv)):
-        object.append(os.sys.argv[i])
-    s(object, filename)
+    save_to_json_file([], "add_item.json")
+thisobject = load_from_json_file("add_item.json")
+my_list = []
+for i in range(1, len(sys.argv)):
+    my_list.append(sys.argv[i])
+thisobject = thisobject + my_list
+save_to_json_file(thisobject, "add_item.json")
