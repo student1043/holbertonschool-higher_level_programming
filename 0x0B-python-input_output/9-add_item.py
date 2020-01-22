776a14cfa303ae2b78cb175e3e0e702702ff9f9a
student1043 <1043@holbertonschool.com>
2020-01-22 23:28:00 +0100

    IT'S FUCKING WORKING CHECKERgit add -A

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 import json
 import os
-loadmedaddy = __import__('8-load_from_json_file').load_from_json_file
-savemedaddy = __import__('7-save_to_json_file').save_to_json_file
+l = __import__('8-load_from_json_file').load_from_json_file
+s = __import__('7-save_to_json_file').save_to_json_file
 
 """
-Save the arguments to the JSON file add_item.json
+Adding arguments to a JSON file add_item.json
 """
-
 
 filename = "add_item.json"
 try:
-    list = loadmedaddy(filename)
+    object = l(filename)
 except:
     with open(filename, mode='w', encoding='UTF8') as pr:
         pr.write('')
-        list = []
+        object = []
 if len(os.sys.argv) == 1:
-    savemedaddy(list, filename)
+    s(object, filename)
 else:
     for i in range(1, len(os.sys.argv)):
-        list.append(os.sys.argv[i])
-    savemedaddy(list, filename)
+        object.append(os.sys.argv[i])
+    s(object, filename)
