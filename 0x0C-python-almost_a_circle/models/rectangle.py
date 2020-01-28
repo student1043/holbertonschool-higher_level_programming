c9c7224552123d43ce561310ee15ec14851006a6
student1043 <1043@holbertonschool.com>
2020-01-28 15:36:40 +0100

    Task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,4 +83,4 @@ class Rectangle(Base):
     def __str__(self):
         """ string """
         return "[Rectangle] ({}) {}/{} - {}/{}"\
-        .format(self.id, self.__x, self.__y, self.__width, self.__height)
+            .format(self.id, self.__x, self.__y, self.__width, self.__height)
