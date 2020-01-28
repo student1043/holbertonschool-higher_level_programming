957e95c20777f31cf1033ceb375b15269ebd81d6
student1043 <1043@holbertonschool.com>
2020-01-28 15:22:11 +0100

    Task 3 and 4

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -36,19 +36,39 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """ width setter """
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
         self.__width = value
 
     @height.setter
     def height(self, value):
         """ height setter """
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
         self.__height = value
 
     @x.setter
     def x(self, value):
         """ x setter """
+        if type(value) is not int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
         self.__x = value
 
     @y.setter
     def y(self, value):
         """ y setter """
+        if type(value) is not int:
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
         self.__y = value
+    
+    def area(self):
+        """ Area of the Rectangle """
+        return(self.__width * self.__height)
