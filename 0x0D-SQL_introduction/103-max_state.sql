072decb583179ae91ca3f590ce82ffea8e962dce
student1043 <1043@holbertonschool.com>
2020-03-02 23:33:44 +0100

    Update 103-max_state.sql

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- Task 103: Max by state
-SELECT state, MAX(value) as max_temp FROM temperatures GROUP BY city ORDER BY max_temp DESC limit 3;
+SELECT state, MAX(value) as max_temp FROM temperatures GROUP BY state ORDER BY max_temp DESC limit 3;
