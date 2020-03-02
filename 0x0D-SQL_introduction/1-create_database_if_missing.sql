7dcb295c9b7cc4990e1f667554247b0064d43dbf
student1043 <1043@holbertonschool.com>
2020-03-02 22:20:42 +0100

    Fixing task 1 / Task 2

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
 -- Task 1: Create a new database if missing
-CREATE DATABASE hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- Task 2: Remove Database
+DROP DATABASE IF EXISTS hbtn_0c_0;
