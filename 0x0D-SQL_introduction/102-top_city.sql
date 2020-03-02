9ed895f999722fe2b0d78e419416be44d1ff32ff
student1043 <1043@holbertonschool.com>
2020-03-02 23:30:08 +0100

    Update 102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- Task 102: Top 3 cities
-SELECT TOP 3 city, AVG(value) as avg_temp FROM temperatures  WHERE (month = 7 and month = 8) GROUP BY city ORDER BY avg_temp DESC;
+SELECT TOP 3 city, AVG(value) as avg_temp FROM temperatures WHERE (month = 7 and month = 8) GROUP BY city ORDER BY avg_temp DESC;
