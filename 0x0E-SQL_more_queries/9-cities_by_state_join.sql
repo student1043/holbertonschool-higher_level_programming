81f21805d664a22c3f7a9b31a1d4a22e2ae7e337
student1043 <1043@holbertonschool.com>
2020-03-03 16:07:48 +0100

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,5 @@
 -- Task 9: all cities and states
 -- Second Line
-SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state.id=states.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities
+INNER JOIN states ON cities.state_id=states.id
+ORDER BY cities.id ASC;
