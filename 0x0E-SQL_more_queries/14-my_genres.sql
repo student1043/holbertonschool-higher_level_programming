05080edb7fca7da43f926755d53194aa7c816561
student1043 <1043@holbertonschool.com>
2020-03-03 17:30:57 +0100

    Task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,4 @@
 -- Task 14: Dexter
 -- Second Line
+USE hbtn_0d_tvshows;
 SELECT tv_genres.name AS name FROM tv_genres WHERE name= (SELECT name FROM tv_shows.title WHERE name='Dexter') INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id GROUP BY name ORDER BY name ASC;
