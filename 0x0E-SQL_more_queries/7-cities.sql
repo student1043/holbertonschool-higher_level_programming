86441aa908721f939b324976afa1b5615534c402
student1043 <1043@holbertonschool.com>
2020-03-03 15:02:38 +0100

    Create 7-cities.sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,4 @@
+-- Task 7: New Database
+-- Second Line
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL AUTO_INCREMENT, state_id INT NOT NULL FOREIGN KEY REFERENCES states(id), name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
