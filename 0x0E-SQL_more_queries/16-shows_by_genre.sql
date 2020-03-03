9ea6f5ecb326c9fa78619b294eef09ebc6fdc36c
student1043 <1043@holbertonschool.com>
2020-03-03 22:34:24 +0100

    Update 16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,3 +1,3 @@
 -- Task 16: list all shows and genres linked to it
 -- Second Line
-SELECT tv_shows.title, tv_genres.name FROM tv_shows WHERE tv_genres.name IS NULL LEFT JOIN tv_genres ON name = tv_shows.title ORDER BY title, tv_genres.name ASC;
+SELECT tv_shows.title, tv_genres.name FROM tv_shows LEFT JOIN tv_genres ON name = tv_shows.title ORDER BY title, tv_genres.name ASC;
