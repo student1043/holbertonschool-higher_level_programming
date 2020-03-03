296935b34fe6f011a9971b14710b4306b6efccc3
student1043 <1043@holbertonschool.com>
2020-03-03 15:50:08 +0100

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,4 @@
 -- Task 9: all cities and states
 -- Second Line
-SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON cities.state.id=states.id ORDER BY id ASC;
+USE hbtn_0d_usa;
+SELECT cities.id,cities.name,states.name FROM cities LEFT JOIN states ON cities.state.id=states.id ORDER BY id ASC;
