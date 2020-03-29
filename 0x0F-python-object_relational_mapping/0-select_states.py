22092d844e892392247426b7ce0d7085afcdb129
student1043 <1043@holbertonschool.com>
2020-03-29 00:17:27 +0000

    Fixing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ List states """
 if __name__ == "__main__":
     import sys
     import MySQLdb
 
-engine = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2], host="localhost", db=sys.argv[3])
-cursor = engine.cursor()
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
-query = cursor.fetchall()
-for row in query:
-    print(row)
-cursor.close()
-engine.close()
+    engine = MySQLdb.connect(user=sys.argv[1],
+                             passwd=sys.argv[2], host="localhost", db=sys.argv[3])
+    cursor = engine.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    query = cursor.fetchall()
+    for row in query:
+        print(row)
+        cursor.close()
+        engine.close()
