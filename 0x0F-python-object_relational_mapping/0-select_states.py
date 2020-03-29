811c62613136471eb62d5ea21b0a79cd771522f5
student1043 <1043@holbertonschool.com>
2020-03-29 00:15:28 +0000

    Fixing Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-import sys
-import MySQLdb
+""" List states """
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
 engine = MySQLdb.connect(user=sys.argv[1],
                          passwd=sys.argv[2], host="localhost", db=sys.argv[3])
 cursor = engine.cursor()
