320fb1f6976757dd75094567658b53351cff70a3
student1043 <1043@holbertonschool.com>
2020-04-09 14:28:52 +0100

    Fixing Task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Task 1
-curl -s $1 -X POST -d "email: hr@holbertonschool.com" -d "subject: I will always be here for PLD"
+curl -s -d "email: hr@holbertonschool.com" -d "subject: I will always be here for PLD" $1
