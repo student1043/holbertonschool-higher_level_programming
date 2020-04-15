1bb667a0dfa3d4b5f7fb38dbb1e81010470a6d9f
student1043 <1043@holbertonschool.com>
2020-04-15 20:38:50 +0100

    Fixing task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,6 +3,7 @@ import requests
 import sys
 """ Requests """
 
-
-r = requests.get(sys.argv[1])
-print(r.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    """ Request """
+    r = requests.get(sys.argv[1])
+    print(r.headers.get('X-Request-Id'))
