11c48d2ab6868f6a575f94c2cb83bc154acd8e00
student1043 <1043@holbertonschool.com>
2020-04-15 20:44:08 +0100

    Fixing Task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,7 +3,6 @@ import requests
 import sys
 """ Requests """
 
-
-r = requests.post(sys.argv[1], data = {'email':sys.argv[2]})
-print(r.text)
-
+if __name__ == "__main__":
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(r.text)
