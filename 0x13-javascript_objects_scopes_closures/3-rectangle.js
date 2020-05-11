6d3784a258986d9bcdc1107ba5bd0e7c57c5f207
student1043 <1043@holbertonschool.com>
2020-05-11 15:37:29 +0100

    Task 3 fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,15 +5,20 @@ class Rectangle {
     } else if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-      this.print = function () {
-        let str = '';
-        for (let i = 0; i < h; i++) {
-          for (let j = 0; j < w; j++) { str += 'X' + ''; }
-          if (i < w) { str += '\n'; }
-        }
-        console.log(str);
-      };
     }
+  }
+
+  print () {
+    let str = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        str += 'X' + '';
+      }
+      if (i < this.width) {
+        str += '\n';
+      }
+    }
+    console.log(str);
   }
 }
 module.exports = Rectangle;
