d997fa4d97afce0a34aa482d2c009a1c815b0c8c
student1043 <1043@holbertonschool.com>
2020-05-12 15:50:20 +0100

    Fixing Task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
-  console.error('error:', error);
-  console.log('code: ' + response.statusCode);
+	if (error) {
+		console.error('error:', error);
+	}
+	console.log('code: ' + response.statusCode);
 });
