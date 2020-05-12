9501ffde8c648b3d8c05470a28b9ccb950787749
student1043 <1043@holbertonschool.com>
2020-05-12 14:51:41 +0100

    Task 0

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const list = require('./100-data').list;
+const newlist = list.map(x => x * list[index]);
+console.log(list);
+console.log(newlist);
---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const fs = require('fs');
+const content = fs.readFileSync(process.argv[2], 'utf8');
+console.log(content);
