2e467e8389778cd5e21ec66d84ccc27917291dfe
student1043 <1043@holbertonschool.com>
2020-05-12 21:53:57 +0100

    Fixing Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+const process = require('process');
 const request = require('request');
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
   if (error) {
