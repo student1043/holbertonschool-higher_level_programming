726e2cf931b887cf15f2a0187a68c9e40e28cbca
student1043 <1043@holbertonschool.com>
2020-07-05 13:02:47 +0100

    Task

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,10 @@
+const $ = window.$;
+$(document).ready(function(){
+	$('INPUT#btn_translate').click(function(){
+		const language = $('INPUT#language_code').val();
+		const link = 'https://www.fourtonfish.com/hellosalut/?lang=' + language;
+		$.getJSON(link, function (data){
+			$('DIV#hello').text(data.hello);
+		});
+	});
+});
