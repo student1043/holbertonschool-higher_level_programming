d1234c5341518845c43c8744690be5faa8ae8e13
student1043 <1043@holbertonschool.com>
2020-07-06 18:49:46 +0100

    Fixing Permissions

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const request = require('request');
 const url = process.argv[2];
 request(url, function (error, response, body) {
-    if (error) {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf8', function (error, data) {
+      if (error) {
         console.log(error);
-    } else {
-        fs.writeFile(process.argv[3], body, 'utf8', function(error, data) {
-            if (error) {
-                console.log(error);
-            }
-        });
-    }
+      }
+    });
+  }
 });
