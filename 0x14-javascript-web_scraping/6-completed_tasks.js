ab4dad506e9a24bf1a1b3adc4ba6b142116dcacc
student1043 <1043@holbertonschool.com>
2020-07-06 19:57:15 +0100

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
-const link = process.argv[2];
+const url = process.argv[2];
 const completed = {};
-request(link, function (error, response, body) {
+request(url, function (error, response, body) {
   if (error) throw error;
   const data = JSON.parse(body);
   for (let i = 0; i < data.length; i++) {
