872dcac7fc3ef03d4cb4a39cb484f704b7bc6ec2
student1043 <1043@holbertonschool.com>
2020-07-06 19:09:20 +0100

    Fixing

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,16 +2,16 @@
 const process = require('process');
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const mylist = JSON.parse(body).results;
-    let number = 0;
-    for (const movie of mylist) {
-      if (movie.characters.find(item => item.includes('/api/people/18/'))) {
-        number++;
-      }
-    }
-    console.log(number);
-  }
+	if (error) {
+		console.log(error);
+	} else {
+		const mylist = JSON.parse(body).results;
+		let number = 0;
+		for (const movie of mylist) {
+			if (movie.characters.find(item => item.includes('/api/people/18/'))) {
+				number++;
+			}
+		}
+		console.log(number);
+	}
 });
