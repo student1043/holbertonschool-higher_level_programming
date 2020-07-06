dbcd6b282797ae971957438d2d93a22420902995
student1043 <1043@holbertonschool.com>
2020-07-06 19:21:29 +0100

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
-const fs = require('fs');
 const request = require('request');
+const fs = require('fs');
 const url = process.argv[2];
 request(url, function (error, response, body) {
-    if (error) {
-        console.log(error);
-    } else {
-        fs.writeFile(process.argv[3], body, 'utf8', function(error, data) {
-            if (error) {
-                console.log(error);
-            }
-        });
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf8', (error) => {
+      if (error) { console.log(error); }
+    });
+  }
 });
