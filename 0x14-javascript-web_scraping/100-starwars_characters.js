e8430e4699d06dead5c301dd9d8a5b7a2cb68c8d
student1043 <1043@holbertonschool.com>
2020-07-06 20:10:02 +0100

    fix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(link, function (error, response, body) {
+request(url, function (error, response, body) {
   if (error) throw error;
   const d = JSON.parse(body).characters;
   for (let i = 0; i < d.length; i++) {
