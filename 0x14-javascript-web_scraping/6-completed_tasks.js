1675baac29727af407a6922c47f38d9c8a291ae6
student1043 <1043@holbertonschool.com>
2020-07-06 19:59:32 +0100

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,13 +4,13 @@ const url = process.argv[2];
 const completed = {};
 request(url, function (error, response, body) {
   if (error) throw error;
-  const data = JSON.parse(body);
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].completed === true) {
-      if (completed[data[i].userId]) {
-        completed[data[i].userId] += 1;
+  const d = JSON.parse(body);
+  for (let i = 0; i < d.length; i++) {
+    if (d[i].completed === true) {
+      if (completed[d[i].userId]) {
+        completed[d[i].userId] += 1;
       } else {
-        completed[data[i].userId] = 1;
+        completed[d[i].userId] = 1;
       }
     }
   }
